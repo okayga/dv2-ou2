eb43ee15489d331c293c450af4e571d055b4cb7f
Larsson <c20mln@ad.cs.umu.se>
2022-03-11 15:25:20 +0100

    added code to freqtable.c

--- huffman/freqtable.c
+++ huffman/freqtable.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
 
-freqtable freqtable_create() {
+freqtable *freqtable_create() {
 
-  freqtable ft = malloc(sizeof(freqtable));
+  freqtable *ft = malloc(sizeof(freqtable));
   for (int i = 0; i < 256; i++) {
     ft.characters[i] = (char)i;
     ft.frequency[i] = 0;
+
   }
+  return ft;
 }
 
-void freqtable_destroy(freqtable ft) {
+void freqtable_destroy(freqtable *ft) {
 
   free(ft);
 }
